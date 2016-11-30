d1ad7bdda27047aaee1c6e08066f2faa836ae16b
Himanshu Dixit <hudixt@gmail.com>
2016-11-30 18:25:30 +0530

    Judge Script Started

---
+++ run.py
@@ -0,0 +1,12 @@
+###############################################
+##     Bezel Online Judge                     #
+##     Version 1.0                            #
+##     Himanshu Dixit,Utkarsh Dixit           #
+##     file - RUN.PY                          #
+###############################################
+
+import os,filecmp
+
+#Response Codes To Be Used Further
+
+response = {1000:'Some Error Occured',1001:'Compilation Error',1002:'Runtime Error',1003:'Memory Limit Exceeded',1004:'Time Limit Exceeded',1005:'Source Limit Exceeded',1006:'Wrong Answer',1007:'Unknown Error'}
