81cdb64b0b95d027e6af5cb884880e4dd10296fa
Himanshu Dixit <hudixt@gmail.com>
2016-11-30 18:55:14 +0530

    Functions Declaration in RUN.PY

--- run.py
+++ run.py
@@ -10,3 +10,20 @@ import os,filecmp
 #Response Codes To Be Used Further
 
 response = {1000:'Some Error Occured',1001:'Compilation Error',1002:'Runtime Error',1003:'Memory Limit Exceeded',1004:'Time Limit Exceeded',1005:'Source Limit Exceeded',1006:'Wrong Answer',1007:'Unknown Error'}
+
+#Create Function To Get Data
+
+def create(code,language,source):
+    print "Processing File Started"
+
+def compiler(code,language,source):
+    print "Processing File Started"
+
+def run(code,language,source):
+    print "Processing File Started"
+
+def check(code,language,source):
+    print "Processing File Started"
+
+def terminate(code,language,source):
+    print "Processing File Started"
