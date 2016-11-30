d6329f325f937e945c9784df1cb912bf10c69a08
Himanshu Dixit <hudixt@gmail.com>
2016-12-01 02:03:16 +0530

    Different Lang Conditions implemented

--- run.py
+++ run.py
@@ -11,13 +11,74 @@ import os,filecmp
 
 response = {1000:'Some Error Occured',1001:'Compilation Error',1002:'Runtime Error',1003:'Memory Limit Exceeded',1004:'Time Limit Exceeded',1005:'Source Limit Exceeded',1006:'Wrong Answer',1007:'Unknown Error'}
 
+#Path to temporaray location
+
+dir = '/temp/code_location'
+
 #Create Function To Get Data
 
-def create(id,code,language,source):
-    
+
+
+def create(id,code,lang,source):
+    if lang = 'c++ 5.1':
+        extn = 'cpp'
+    else if lang = 'c++ 14.1'
+        extn = 'cpp'
+    else if lang = 'bash'
+        extn = 'sh'
+    else if lang = 'java'
+        extn = 'java'
+    else if lang = 'java7'
+        extn = 'java'
+    else if lang = 'haskell'
+        extn = 'hs'
+    else if lang = 'pascalfpc'
+        extn = 'pas'
+    else if lang = 'pascalgpc'
+        extn = 'pas'
+    else if lang = 'perl'
+        extn = 'pl'
+    else if lang = 'python2.7'
+        extn = 'py'
+    else if lang = 'python3.4'
+        extn = 'py'
+    else if lang = 'ruby'
+        extn = 'rb'
+    else if lang = 'javascript'
+        extn = 'js'
+    else if lang = 'brainfuck'
+        extn = 'bf'
+
 
 def compiler(code,language,source):
-    print "Processing File Started"
+        if lang = 'c++ 5.1':
+            extn = 'cpp'
+        else if lang = 'c++ 14.1'
+            extn = 'cpp'
+        else if lang = 'bash'
+            extn = 'sh'
+        else if lang = 'java'
+            extn = 'java'
+        else if lang = 'java7'
+            extn = 'java'
+        else if lang = 'haskell'
+            extn = 'hs'
+        else if lang = 'pascalfpc'
+            extn = 'pas'
+        else if lang = 'pascalgpc'
+            extn = 'pas'
+        else if lang = 'perl'
+            extn = 'pl'
+        else if lang = 'python2.7'
+            extn = 'py'
+        else if lang = 'python3.4'
+            extn = 'py'
+        else if lang = 'ruby'
+            extn = 'rb'
+        else if lang = 'javascript'
+            extn = 'js'
+        else if lang = 'brainfuck'
+            extn = 'bf'
 
 def run(code,language,source):
     print "Processing File Started"
